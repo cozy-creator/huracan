8b1b774b37ca6487943e717ab68ee9e98fa04509
sheldonkreger <sheldonkreger@outlook.com>
2023-08-28 18:48:27 -0700

    Syntax

--- main/src/conf.rs
+++ main/src/conf.rs
@@ -210,11 +210,15 @@ impl AppConfig {
 }
 
 // Singleton for config
-pub(crate) static APPCONFIG: OnceCell<AppConfig> = OnceCell::new();
+pub(crate) static APPCONFIG: OnceCell<AppConfig> = OnceCell::const_new();
 
 // Setup config singleton
 pub async fn setup_config_singleton(cfg: &AppConfig) -> Result<&'static AppConfig, String> {
-	APPCONFIG.get_or_try_init(|| Ok(cfg.clone())).await.expect("ConfigError: Unable to initialize config singleton");
+	APPCONFIG.get_or_try_init(|| async {
+		Ok.cfg.clone()
+	}).await.expect("ConfigError: Unable to initialize config singleton");
+
+	// APPCONFIG.get_or_try_init(|| Ok(cfg.clone())).await.expect("ConfigError: Unable to initialize config singleton");
 	Ok(APPCONFIG.get().unwrap())
 }
 
