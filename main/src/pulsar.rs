7c3dc5b51790c18d16caab4cd9ec63ebc6f11021
sheldonkreger <sheldonkreger@outlook.com>
2023-09-10 12:22:46 -0700

    Initial commit for Pulsar client singleton

--- main/src/pulsar.rs
+++ main/src/pulsar.rs
@@ -2,8 +2,10 @@ use pulsar::{
 	authentication::oauth2::{OAuth2Authentication, OAuth2Params},
 	Producer, Pulsar, TokioExecutor,
 };
+use tokio::sync::OnceCell;
 
 use crate::_prelude::*;
+use crate::conf::get_config_singleton;
 
 pub async fn make_producer(
 	cfg: &AppConfig,
@@ -19,6 +21,20 @@ pub async fn make_producer(
 		.await?)
 }
 
+pub async fn make_transaction_producer(
+	pulsar: &Pulsar<TokioExecutor>,
+	ty: &str,
+) -> anyhow::Result<Producer<TokioExecutor>> {
+	let cfg = get_config_singleton();
+	Ok(pulsar
+        .producer()
+        // e.g. {persistent://public/default/}{prod}_{testnet}_{objects}_{retries}
+        // braces added for clarity of discerning between the different parts
+        .with_topic(&format!("{}{}_{}_{}_", cfg.pulsar.transaction_topicbase, cfg.env, cfg.net, ty))
+        .build()
+        .await?)
+}
+
 pub async fn create(cfg: &AppConfig) -> anyhow::Result<Pulsar<TokioExecutor>> {
 	Ok(Pulsar::builder(&cfg.pulsar.url, TokioExecutor)
 		.with_auth_provider(OAuth2Authentication::client_credentials(OAuth2Params {
@@ -29,4 +45,16 @@ pub async fn create(cfg: &AppConfig) -> anyhow::Result<Pulsar<TokioExecutor>> {
 		}))
 		.build()
 		.await?)
+}
+
+pub(crate) static PULSARCLIENT: OnceCell<Pulsar<TokioExecutor>> = OnceCell::const_new();
+
+// Setup config singleton
+pub async fn setup_pulsar_singleton() -> anyhow::Result<Pulsar<TokioExecutor>> {
+	let cfg = get_config_singleton();
+	PULSARCLIENT.get_or_init(|| async { create(cfg) }).await.unwrap()
+}
+
+pub fn get_pulsar_singleton() -> Pulsar<TokioExecutor> {
+	PULSARCLIENT.get().expect("ConfigError: Pulsar Client singleton could not be loaded.").unwrap()
 }
